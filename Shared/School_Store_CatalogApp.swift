cedd321e3b61124da3361e09834f2eafe9f5f177
Bradley Kang <bkang@lcs.on.ca>
2022-03-25 11:24:54 -0400

    Initial Commit

---
+++ Shared/School_Store_CatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  School_Store_CatalogApp.swift
+//  Shared
+//
+//  Created by Brad Kang on 2022-03-25.
+//
+
+import SwiftUI
+
+@main
+struct School_Store_CatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
