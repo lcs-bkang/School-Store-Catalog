d19d60ea0e755ad46982243593d08998822ca0b6
Bradley Kang <bkang@lcs.on.ca>
2022-03-31 08:58:35 -0400

    Added image names and JSON and structure changes to reflect the added images.  Changed exampledata as well

--- Shared/Models/Catalog.swift
+++ Shared/Models/Catalog.swift
@@ -10,8 +10,9 @@ struct Catalog: Decodable, Identifiable {
     let item: String
     let cost: Double
     let category: String
+    let image: String
     let colours: [String]
     let id: Int
     
-    static let exampleData = Catalog(item: "Blazar", cost: 55.95, category: "Clothes", colours: ["Green", "Navy"], id: 23)
+    static let exampleData = Catalog(item: "Blazar", cost: 55.95, category: "Clothes", image: "Gum", colours: ["Green", "Navy"], id: 23)
 }
